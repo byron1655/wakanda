3e275b009671787f7a43728fd05d53ccede19a4f
baiyong <baiyong@mafengwo.com>
2019-06-15 23:25:08 +0800

    v0.0.2

--- tools/src/main/java/org/wakanda/tools/ToolsApplication.java
+++ tools/src/main/java/org/wakanda/tools/ToolsApplication.java
@@ -3,9 +3,6 @@ package org.wakanda.tools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 //@Configuration
@@ -15,13 +12,4 @@ public class ToolsApplication {
     public static void main(String[] args) {
         SpringApplication.run(ToolsApplication.class, args);
     }
-
-//    @Configuration
-//    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http.authorizeRequests().anyRequest().permitAll()
-//                    .and().csrf().disable();
-//        }
-//    }
 }
